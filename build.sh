0930987931686ba4bf27cff3b4815d3364705766
Karl Hendrik <karlhendrik.soone@proton.me>
2022-07-22 19:18:27 +0300

    Rerun Travis #1

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ NETWORK_NAME=$(docker network ls --format "{{.Name}}" | grep nginx)
 CONTAINER_NAME=$(docker ps --format "{{.Names}}" | grep gists_pipe_app)
 
 echo "Building..."
-docker-compose -f "$PROJECT_ROOT"/docker-compose.yml up --build -d
 echo "Replacing default placeholder with $APP_URL and connect to network $NETWORK_NAME"
 sed -i "s/http:\/\/localhost:8000/${APP_URL}/g" "$PROJECT_ROOT"/app/static/js/main.js
-docker network connect "$NETWORK_NAME" "$CONTAINER_NAME"
+docker-compose -f "$PROJECT_ROOT"/docker-compose.yml up --build -d
+docker network connect "$NETWORK_NAME" "$CONTAINER_NAME" 
